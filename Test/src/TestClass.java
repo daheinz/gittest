b4593dd0623ccb9fd1fbb11129b94a0555a355cd
David Heinz <Heinz.David@gmail.com>
2018-02-03 09:45:20 -0600

    Initial Commmit

---
+++ Test/src/TestClass.java
@@ -0,0 +1,22 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 
+ */
+
+/**
+ * @author heinz
+ *
+ */
+public class TestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+	}
+
+}
